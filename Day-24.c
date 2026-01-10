6e3583a987d32156275151272886c9fbc668fba2
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-01-10 15:53:19 +0600

    Update Day-24.c

--- Day-24.c
+++ Day-24.c
@@ -124,10 +124,85 @@ printf("%d dia(s)\n",day);
     
 
 
+// bee 1021
+
+double n;
+scanf("%lf",&n);
+
+n = n*100;
+int convert = n;
+printf("NOTAS:\n");
+
+int value,taka100,taka50,taka20,taka10,taka5,taka2,taka1,taka050,taka025,taka010,taka005,taka001;
+
+taka100 = convert / 10000;
+printf("%d nota(s) de R$ 100.00\n",taka100);
+
+value = convert%10000;
+
+taka50 = value/5000;
+printf("%d nota(s) de R$ 50.00\n",taka50);
+
+value = value%5000;
+
+taka20 = value/2000;
+printf("%d nota(s) de R$ 20.00\n",taka20);
+
+value = value%2000;
+
+taka10 = value/1000;
+printf("%d nota(s) de R$ 10.00\n",taka10);
+
+value = value%1000;
+
+taka5 = value/500;
+printf("%d nota(s) de R$ 5.00\n",taka5);
+
+value = value%500;
+
+taka2 = value/200;
+printf("%d nota(s) de R$ 2.00\n",taka2);
+
+printf("MOEDAS:\n");
+
+taka1 = value/100;
+printf("%d moeda(s) de R$ 1.00\n",taka1);
+
+value = value%100;
+
+taka050 = value/50;
+printf("%d moeda(s) de R$ 0.50\n",taka050);
+
+value = value%50;
+
+taka025 = value/25;
+printf("%d moeda(s) de R$ 0.25\n",taka025);
+
+value = value%25;
+
+taka010 = value/10;
+printf("%d moeda(s) de R$ 0.10\n",taka010);
+
+value = value%10;
+
+taka005 = value/5;
+printf("%d moeda(s) de R$ 0.05\n",taka005);
+
+value = value%5;
+
+taka001 = value/1;
+printf("%d moeda(s) de R$ 0.01\n",taka001);
+    
+
+
+
+    
+
 
     return 0; 
 
 }
+
 
 
 
